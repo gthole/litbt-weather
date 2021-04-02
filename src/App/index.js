d023da745e3021333968f195204984faa420dc0a
Greg Thole <gthole@gmail.com>
2021-04-01 20:12:20 -0400

    useEffect for data loading & NWS retry

--- src/App/index.js
+++ src/App/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { SearchBar } from './components/SearchBar';
 import { NavBar } from './components/NavBar';
 import { Alerts } from './components/Alerts';
@@ -10,59 +10,61 @@ import { Loading } from './common/Loading';
 import { getForecast } from '../services/nws';
 import './style.css';
 
+function getUserLocation() {
+    const rawLocation = localStorage.getItem('userLocation');
+    if (rawLocation) {
+       return JSON.parse(rawLocation);
+    }
+}
+
 function App() {
-    const [loading, setLoading] = useState(true);
-    const [userLocation, setUserLocation] = useState(null);
-    const [forecast, setForecast] = useState(null);
     const [error, setError] = useState(null);
+    const [state, setState] = useState({
+        userLocation: getUserLocation(),
+        loading: false,
+        forecast: null,
+    });
 
     // Accept a user location change and load forecast data
-    function saveAndSetUserLocation(ul) {
+    function saveAndSetUserLocation(ul)  {
         localStorage.setItem('userLocation', JSON.stringify(ul));
-        setUserLocation(ul);
-        setForecast(null);
-        loadForecast(ul);
+        setState({
+            userLocation: ul,
+            loading: false,
+            forecast: null,
+        });
     }
 
     // Load the forecast from NWS, and set refresh on browser focus (usually cached)
-    function loadForecast(ul) {
-        if (!loading) setLoading(true);
+    useEffect(() => {
+        if (!state.userLocation || state.loading || state.forecast) return;
 
-        getForecast(ul)
-            .then((forecast) => {
-                setForecast(forecast);
-                setLoading(false);
-                window.onfocus = () => loadForecast(ul);
-            })
-            .catch((err) => {
-                setError('Could not load the forecast. Try again later.');
-            })
-    }
-
-    // Load an initial location from local storage
-    if (!userLocation) {
-        const rawLocation = localStorage.getItem('userLocation');
-        if (rawLocation) {
-            saveAndSetUserLocation(JSON.parse(rawLocation));
-            return '';
+        function loadForecast() {
+            if (!state.loading) setState({...state, loading: true});
+            getForecast(state.userLocation)
+                .then((forecast) => setState({...state, forecast, loading: false}))
+                .catch((err) => setError('Could not load the forecast. Try again later.'))
         }
-    }
+
+        window.onfocus = () => loadForecast();
+        loadForecast();
+    }, [state]);
 
     return (
         <div className="App">
             <ErrorAlert body={error} dismiss={ () => setError(null) }/>
             <NavBar />
             <SearchBar
-                userLocation={userLocation}
+                userLocation={state.userLocation}
                 setUserLocation={saveAndSetUserLocation}
-                setLoading={setLoading}
+                setLoading={(loading) => setState({...state, loading})}
                 setError={setError}
             ></SearchBar>
-            <Loading show={userLocation && !forecast && loading}/>
-            <div className={'app-content container ' + (loading ? 'loading' : 'not-loading')}>
-                <Alerts forecast={forecast}></Alerts>
-                <Current forecast={forecast}></Current>
-                <Extended forecast={forecast}></Extended>
+            <Loading show={state.userLocation && !state.forecast && state.loading}/>
+            <div className={'app-content container ' + (state.loading ? 'loading' : 'not-loading')}>
+                <Alerts forecast={state.forecast}></Alerts>
+                <Current forecast={state.forecast}></Current>
+                <Extended forecast={state.forecast}></Extended>
             </div>
             <Footer />
         </div>
