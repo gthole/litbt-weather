8a540fa32f40a997c97a081e9f90eee949a5fe4a
Greg Thole <gthole@gmail.com>
2021-04-25 14:16:24 -0400

    Switch back to separate state declarations

--- src/App/index.js
+++ src/App/index.js
@@ -13,58 +13,58 @@ import './style.css';
 function getUserLocation() {
     const rawLocation = localStorage.getItem('userLocation');
     if (rawLocation) {
-       return JSON.parse(rawLocation);
+        return JSON.parse(rawLocation);
     }
 }
 
 function App() {
     const [error, setError] = useState(null);
-    const [state, setState] = useState({
-        userLocation: getUserLocation(),
-        loading: false,
-        forecast: null,
-    });
+    const [userLocation, setUserLocation] = useState(getUserLocation());
+    const [loading, setLoading] = useState(false);
+    const [forecast, setForecast] = useState(null);
 
     // Accept a user location change and load forecast data
     function saveAndSetUserLocation(ul)  {
         localStorage.setItem('userLocation', JSON.stringify(ul));
-        setState({
-            userLocation: ul,
-            loading: false,
-            forecast: null,
-        });
+        setUserLocation(ul);
+        setLoading(false);
+        setForecast(null);
     }
 
     // Load the forecast from NWS, and set refresh on browser focus (usually cached)
     useEffect(() => {
-        if (!state.userLocation || state.loading || state.forecast) return;
+        if (!userLocation || loading || forecast) return;
 
         function loadForecast() {
-            if (!state.loading) setState({...state, loading: true});
-            getForecast(state.userLocation)
-                .then((forecast) => setState({...state, forecast, loading: false}))
+            if (loading) return;
+            setLoading(true);
+            getForecast(userLocation)
+                .then((forecast) => {
+                    setLoading(false);
+                    setForecast(forecast);
+                })
                 .catch((err) => setError('Could not load the forecast. Try again later.'))
         }
 
         window.onfocus = () => loadForecast();
         loadForecast();
-    }, [state]);
+    }, [userLocation, loading, forecast]);
 
     return (
         <div className="App">
             <ErrorAlert body={error} dismiss={ () => setError(null) }/>
             <NavBar />
             <SearchBar
-                userLocation={state.userLocation}
+                userLocation={userLocation}
                 setUserLocation={saveAndSetUserLocation}
-                setLoading={(loading) => setState({...state, loading})}
+                setLoading={(l) => setLoading(l)}
                 setError={setError}
             ></SearchBar>
-            <Loading show={state.userLocation && !state.forecast && state.loading}/>
-            <div className={'app-content container ' + (state.loading ? 'loading' : 'not-loading')}>
-                <Alerts forecast={state.forecast}></Alerts>
-                <Current forecast={state.forecast}></Current>
-                <Extended forecast={state.forecast}></Extended>
+            <Loading show={userLocation && !forecast && loading}/>
+            <div className={'app-content container ' + (loading ? 'loading' : 'not-loading')}>
+                <Alerts forecast={forecast}></Alerts>
+                <Current forecast={forecast}></Current>
+                <Extended forecast={forecast}></Extended>
             </div>
             <Footer />
         </div>
